367accba1a2a3124875497ac6167e61efbceef19
Joshua030 <Joseluy123>
2023-05-15 18:11:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset=<?php bloginfo("charset");?>>
+<?php wp_head(); ?>
+<style></style>
+</head>
+<body <?php body_class('example'); ?>>
+  <?php wp_body_open(); ?>
+  <P>hELLO wORLD!</P>
+  <?php wp_footer(); ?>
+</body>
+</html>
